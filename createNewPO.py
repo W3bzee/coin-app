a4ef1fcea2eef6b59e1003f877ccf7d6c7d28895
Chelsey Fang <chelsey.fang@gmail.com>
2023-02-26 18:01:26 -0800

    gitignore

--- createNewPO.py
+++ createNewPO.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 
 def createNewPOfunc():
-    return pd.DataFrame(columns={'PCGS #','Description','Grade','Service','CAC','Cost','Price','Certification ID','Addl. Description'},index=np.arange(1,15))
+    return pd.DataFrame(columns=['PCGS #','Description','Grade','Service','CAC','Cost','Price','Certification ID','Addl. Description'],index=np.arange(1,15))
 
 print(createNewPOfunc())
