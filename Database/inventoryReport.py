eca04ec4dee6b6322d3780f9597b64237b17e338
W3bzee <103391878+W3bzee@users.noreply.github.com>
2023-06-05 22:38:12 -0700

    add $'s to inventory report

--- Database/inventoryReport.py
+++ Database/inventoryReport.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import locale
+locale.setlocale( locale.LC_ALL, '' )
+
 
 def getReportData():
     countPO = pd.read_csv('Database\Data\purchaseOrders.csv').shape[0]-1
     countInvoices = pd.read_csv('Database\Data\invoices.csv').shape[0]-1
     countCoinsInventory = pd.read_csv('Database\Data\inventoryCoins.csv').shape[0]
-    pricePaid = pd.read_csv('Database\Data\purchaseOrderCoins.csv')['Cost'].sum()
-    priceSold = pd.read_csv('Database\Data\invoiceCoins.csv')['Price'].sum()
-    
+    pricePaid = locale.currency(pd.read_csv('Database\Data\purchaseOrderCoins.csv')['Cost'].sum(),grouping=True )
+    priceSold = locale.currency(pd.read_csv('Database\Data\invoiceCoins.csv')['Price'].sum(),grouping=True )
+
     return countPO, countInvoices, countCoinsInventory, pricePaid, priceSold
     
